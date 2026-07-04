c074903c6f88d38f5315024a84ee304548ff7875
roshnn-sahu <locroshan@gmail.com>
2026-07-04 13:03:40 +0530

    fix(layout-container): adjust max-width for component slug and fix div visibility

--- components/layout-container.tsx
+++ components/layout-container.tsx
@@ -13,12 +13,12 @@ export default function LayoutContainer({
   return (
     <div
       className={`relative container mx-auto border border-x border-border ${
-        isComponentSlug ? "max-w-6xl" : "md:max-w-170"
+        isComponentSlug ? "max-w-4xl" : "md:max-w-170"
       }`}
     >
-      <div className="hidden md:flex absolute top-0 -left-6 z-50 h-full w-6 border-x border-x-(--pattern-fg) bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed opacity-80 [--pattern-fg:var(--color-neutral-300)] dark:opacity-12" />
+      <div className="absolute top-0 -left-6 z-50 hidden h-full w-6 border-x border-x-(--pattern-fg) bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed opacity-80 [--pattern-fg:var(--color-neutral-300)] md:flex dark:opacity-12" />
       {children}
-      <div className="hidden md:flex absolute top-0 -right-6 z-50 h-full w-6 border-x border-x-(--pattern-fg) bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed opacity-80 [--pattern-fg:var(--color-neutral-300)] dark:opacity-12" />
+      <div className="absolute top-0 -right-6 z-50 hidden h-full w-6 border-x border-x-(--pattern-fg) bg-[repeating-linear-gradient(315deg,var(--pattern-fg)_0,var(--pattern-fg)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] bg-fixed opacity-80 [--pattern-fg:var(--color-neutral-300)] md:flex dark:opacity-12" />
     </div>
   );
 }
